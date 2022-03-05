cc7f145a27fbb339a4f82c50552ccd979619bb18
monsoonery <masonchammat@gmail.com>
2022-03-06 00:55:01 +0100

    Updated with stick press functionality
    Pressing the joystick on the transmitting Arduino now turns on a LED on the receiving Arduino. Also added clean comments

--- component_tests/joystick_DCmotor_control_receiver/joystick_DCmotor_control_receiver.ino
+++ component_tests/joystick_DCmotor_control_receiver/joystick_DCmotor_control_receiver.ino
@@ -1,63 +1,102 @@
-//Include Libraries
+// libraries
 #include <SPI.h>
 #include <RF24.h>
 
-//create an RF24 object
-RF24 radio(9, 10);  // Arduino pin 9 is CE, pin 10 is CSN
-const byte pipeName[6] = "99999";
+RF24 radio(9, 10);                           // create an RF24 object, pin 9 is CE, pin 10 is CSN
+const byte pipeName[6] = "99999";            // our custom pipe adress
+byte payload[32] = {0};                      // array of bytes that will be sent over RF
 
-const int centerX = 124;
-int VRX = centerX;
+// values corresponding to neutral position of joystick
+const int centerX = 124;                     
+const int centerY = 129;
+
+// pin definitions
+const int ledPin = 4;
+const int forwardPin = 5;
+const int backwardPin = 6;
+
+// payload values
+int VRX = centerX;                           // [0] init to neutral position
+int VRY = centerY;                           // [1] init to neutral position
+int GRX = centerX;                           // [2] init to neutral position
+int GRY = centerY;                           // [3] init to neutral position
+bool VRS = false;                            // [4] init to button non-pressed
+bool GRS = false;                            // [5] init to button non-pressed
 
 void setup() {
-  // put your setup code here, to run once:
-  radio.begin();      // setup myRadio module
-  radio.openReadingPipe(0, pipeName);  // the radio can listen to 6 transmitters, we only use one.
+  //Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  
+  radio.begin();                             // setup myRadio module
+  radio.openReadingPipe(0, pipeName);
   radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+  radio.startListening();                    // this is a receiver
 }
 
 void loop() {
-  // Storage for the received data. First it is emptied
-  byte payload[32] = {0};
+  checkRadio();                              // check for new incoming transmissions
+  updateWheels();                            // actuate wheels according to transmission data
+  updateLed();                               // power LED according to transmission data
+}
 
+
+void checkRadio() {
   if (radio.available()) {
     Serial.print("receiving data: ");
     radio.read(&payload, sizeof(payload));
     for (int i = 0; i < 32; i++) {
-      Serial.print(payload[i]);
-      Serial.print("  ");
+      Serial.print(payload[i]); Serial.print("  ");
     }
     Serial.println();
-
-    VRX = payload[0];
-    updateWheels();
+    unpackPayload();                         // put data in variables for further processing
   }
 }
 
+void unpackPayload() {
+  // x and y values from V-joystick
+  VRX = payload[0];
+  VRY = payload[1];
+  // x and y values from G-joystick
+  GRX = payload[2];
+  GRY = payload[3];
+  // button press state from joysticks
+  VRS = payload[4]; 
+  GRS = payload[5];
+}
+
 void updateWheels() {
-  // als joystick omhoog gericht, dan moet hij naar voren rijden, dus gebruik pin 5
+  // als joystick omhoog gericht, dan moet hij naar voren rijden, dus gebruik backwardPin
   if (VRX < centerX) {
     int estPower = (centerX - VRX) * 2;
     int power = constrain(estPower, 0, 255);
-    analogWrite(5, power);
-    analogWrite(6, 0);
+    analogWrite(backwardPin, power);
+    analogWrite(forwardPin, 0);
     Serial.print(power);
     Serial.println(" -- forward");
-    // als joystick omlaag gericht, dan moet hij naar achter rijden, dus gebruik pin 5
+    // als joystick omlaag gericht, dan moet hij naar achter rijden, dus gebruik pin backwardPin
   } else if (VRX > centerX) {
     int estPower = (VRX - centerX) * 2;
     int power = constrain(estPower, 0, 255);
-    analogWrite(5, 0);
-    analogWrite(6, power);
+    analogWrite(backwardPin, 0);
+    analogWrite(forwardPin, power);
     Serial.print(power);
     Serial.print(" -- backward");
     // als joystick in neutrale positie, dan moet hij niet rijden
   } else {
-    analogWrite(5, 0);
-    analogWrite(6, 0);
+    analogWrite(backwardPin, 0);
+    analogWrite(forwardPin, 0);
     Serial.print(0);
     Serial.print("neutral");
   }
   delay(10);
+}
+
+void updateLed() {
+  // led turns on when joystick is pressed
+  digitalWrite(ledPin, VRS ? HIGH : LOW);
+  if (VRS) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
 }
--- component_tests/joystick_DCmotor_control_transmitter/joystick_DCmotor_control_transmitter.ino
+++ component_tests/joystick_DCmotor_control_transmitter/joystick_DCmotor_control_transmitter.ino
@@ -1,37 +1,54 @@
-//Include Libraries
+// libraries
 #include <SPI.h>
 #include <RF24.h>
 
-//create an RF24 object
-RF24 radio(9, 10);  // Arduino pin 9 is CE, pin 10 is CSN
-const byte pipeName[6] = "99999";
+// radio communications
+RF24 radio(9, 10);                              // create an RF24 object, pin 9 is CE, pin 10 is CSN
+const byte pipeName[6] = "99999";               // our custom pipe address
+byte payload[32] = {0};                         // array of bytes that will be sent over RF
 
-int x1Pin = A1;
-int y1Pin = A0;
+// pin definitions
+const int x1Pin = A1;                           // x-position of joystick
+const int y1Pin = A0;                           // y-position of joystock
+const int button1Pin = 4;                       // joystick button
+
+// payload values
+int VRX, 
+    VRY;
+bool buttonPress;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(pipeName); //use this pipe name
+  pinMode(button1Pin, INPUT_PULLUP);            // use internal 10k pullup resistor for joystick button
+  radio.begin();                                // setup myRadio module
+  radio.openWritingPipe(pipeName);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();     //Set module as transmitter
+  radio.stopListening();                        // this is a transmitter
 }
 
 void loop() {
-  // The contents of this array of bytes gets transported; it starts filled with zeros
-  byte payload[32] = {0};
+  checkInputs();                                // check for new controller inputs
+  sendRadio();                                  // send inputs over RF
+  delay(10);                                    // wait before next loop
+}
 
-  // take the position of the joysticks connected to analog pins
-  int VRX = analogRead(x1Pin);
-  int VRY = analogRead(y1Pin);
-  Serial.println(VRX);
+void checkInputs() {
+  VRX = analogRead(x1Pin);                      // read position of the joysticks
+  VRY = analogRead(y1Pin);
+  buttonPress = !digitalRead(button1Pin);       // read button state
+  // TODO: implement debounce
+  preparePayload();                             // put values in array for sending over RF
+}
 
+void preparePayload() {
+  // map joystick range to byte-sized range
   payload[0] = map(VRX, 0, 1023, 0, 255);
   payload[1] = map(VRY, 0, 1023, 0, 255);
+  // button state
+  payload[4] = buttonPress;
+}
 
-  Serial.print(payload[0]);
-  Serial.println(" -- sending payload");
+void sendRadio() {
+  Serial.print("sending payload -- "); Serial.println(payload[4]);
   radio.write(&payload, sizeof(payload));
-  delay(10);
 }
