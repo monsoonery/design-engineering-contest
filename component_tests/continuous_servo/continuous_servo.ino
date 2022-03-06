8a7187879fb71c1a68a27fd197a3acc30b9696b0
monsoonery <masonchammat@gmail.com>
2022-03-07 00:11:33 +0100

    Added servoPin variable
    Added variable for servo pin on driver

--- component_tests/continuous_servo/continuous_servo.ino
+++ component_tests/continuous_servo/continuous_servo.ino
@@ -5,6 +5,8 @@
 
 ServoDriver servo;                            // servodriver object
 
+const int servoPin = 1;                       // pin number on DRIVER, NOT on Arduino board
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();                               // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -14,17 +16,17 @@ void setup() {
 
 void loop() {
   for (int i = 90; i <= 270; i++) {
-    servo.setAngle(1, i);                     // loop through all values
+    servo.setAngle(servoPin, i);                     // loop through all values
     Serial.println(i);
     delay(50);
   }
-  servo.setAngle(1, 270);                     // maximum rotation speed for 1 second
+  servo.setAngle(servoPin, 270);                     // maximum rotation speed for 1 second
   Serial.println("max linksom");
   delay(1000);
-  servo.setAngle(1, 180);                     // no rotation, neutral position for 1 second
+  servo.setAngle(servoPin, 180);                     // no rotation, neutral position for 1 second
   Serial.println("neutral");
   delay(1000);
-  servo.setAngle(1, 90);                      // maximum rotation speed for 1 second (opposite direction)
+  servo.setAngle(servoPin, 90);                      // maximum rotation speed for 1 second (opposite direction)
   Serial.println("max rechtsom");
   delay(1000);
 }
