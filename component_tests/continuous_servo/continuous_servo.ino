04d5b63cadeb0ad8dc7db979e102a286cf16bf8a
monsoonery <masonchammat@gmail.com>
2022-03-08 11:37:38 +0100

    Update continuous_servo.ino
    Added extra lines to test two servos at the same time and compare their speed

--- component_tests/continuous_servo/continuous_servo.ino
+++ component_tests/continuous_servo/continuous_servo.ino
@@ -6,6 +6,7 @@
 ServoDriver servo;                            // servodriver object
 
 const int servoPin = 1;                       // pin number on DRIVER, NOT on Arduino board
+const int servoPin2 = 2;                      // pin number on DRIVER, NOT on Arduino board
 
 void setup() {
   Serial.begin(9600);
@@ -17,16 +18,20 @@ void setup() {
 void loop() {
   for (int i = 90; i <= 270; i++) {
     servo.setAngle(servoPin, i);                     // loop through all values
+    servo.setAngle(servoPin2, i);
     Serial.println(i);
     delay(50);
   }
   servo.setAngle(servoPin, 270);                     // maximum rotation speed for 1 second
+  servo.setAngle(servoPin2, 270);
   Serial.println("max linksom");
   delay(1000);
   servo.setAngle(servoPin, 180);                     // no rotation, neutral position for 1 second
+  servo.setAngle(servoPin2, 180);
   Serial.println("neutral");
   delay(1000);
   servo.setAngle(servoPin, 90);                      // maximum rotation speed for 1 second (opposite direction)
+  servo.setAngle(servoPin2, 90); 
   Serial.println("max rechtsom");
   delay(1000);
 }
