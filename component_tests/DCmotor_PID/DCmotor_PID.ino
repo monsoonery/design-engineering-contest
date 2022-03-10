35c6520ed3e079f030682aafe9344e33bbd1765b
monsoonery <masonchammat@gmail.com>
2022-03-11 00:12:14 +0100

    Update DCmotor_PID.ino

--- component_tests/DCmotor_PID/DCmotor_PID.ino
+++ component_tests/DCmotor_PID/DCmotor_PID.ino
@@ -1,21 +1,30 @@
 #include <SPI.h>
+#include <CircularBuffer.h>
 
+/* BUFFER DEFINITIONS */
+const int bufferSize = 10;
+CircularBuffer <float, bufferSize> circularBufferL;
+CircularBuffer <float, bufferSize> circularBufferR;
 
-//CircularBuffer <float, 10> circularBufferL;
-//CircularBuffer <float, 10> circularBufferR;
-int bufferSize = 10;
+/* PIN DEFINITIONS */
+const uint8_t forwardPinL = 9;
+const uint8_t backwardPinL = 8;
+const uint8_t forwardPinR = 5;
+const uint8_t backwardPinR = 6;
 
-const int forwardPinL = 5;
-const int backwardPinL = 6;
-const int forwardPinR = 8;
-const int backwardPinR = 9;
+const uint8_t sensorPinL = 2;
+const uint8_t sensorPinR = 3;
 
-const int sensorPinL = 2;
-const int sensorPinR = 3;
+const int joystickPin_Y1 = A0;
 
-const int diameterWiel = 0.213; //in meters
-const int holes = 12; //aantal gaten in encoder
+/* CONSTANTS */
+const float diameterWiel = 0.213; //in meters
+const uint8_t nHoles = 12;          //aantal gaten in encoder
+const int lowerBoundJS = 520;   //safe low boundary of joystick
+const int upperBoundJS = 526;   //safe upper boundary of joystick
+const uint8_t minimumPower = 70;    //minimum PWM value required to force the wheels to move. used in constrain() function
 
+/* VARIABLES */
 unsigned long timeL1 = 0,
               timeL2 = 0,
               timeR1 = 0,
@@ -23,7 +32,18 @@ unsigned long timeL1 = 0,
 float timeIntervalL = 0,
       timeIntervalR = 0;
 
-const int joystickPin_Y1 = A0;
+unsigned long timeInt1 = 0,
+              timeInt2 = 0;
+
+float errorL = 0,
+      errorPreviousL = 0,
+      integralL = 0,
+      derivativeL = 0,
+      LKp = 0.5,
+      LKi = 0,
+      LKd = 0;
+float total = 0;
+
 
 void setup() {
   Serial.begin(9600);
@@ -31,48 +51,121 @@ void setup() {
   //set up interrupts for infrared sensors
   attachInterrupt(digitalPinToInterrupt(sensorPinL), interruptL, RISING);
   //attachInterrupt(digitalPinToInterrupt(sensorPinR), interruptR, RISING);
+
+  //fill buffers with zeroes
+  for (int i = 0; i < bufferSize; i++) {
+    circularBufferL.push(0);
+    circularBufferR.push(0);
+  }
 }
 
 void loop() {
+  // set desired speed direction variables to zero
   int desiredSpeedForward = 0;
   int desiredSpeedBackward = 0;
 
-
+  // read position of analog stick
   int joystickInput = analogRead(joystickPin_Y1);
-  //Serial.println(joystickInput);
-  if (joystickInput < 522) {
-    desiredSpeedBackward = map(joystickInput, 522, 0, 0, 1200);
+  //Serial.print("joystickInput: "); Serial.println(joystickInput);
+
+  // map analog input from joystick to a desired speed value between 0 and 1200 degrees/second
+  // only map the speed to the desired direction variable (forward or backward)
+  // if joystick isnt being touched: do nothing because there's no desired speed or direction
+  if (joystickInput < lowerBoundJS) {
+    desiredSpeedBackward = map(joystickInput, lowerBoundJS, 0, 0, 1200);
     //Serial.println("pass backward");
-  } else {
-    desiredSpeedForward = map(joystickInput, 522, 1024, 0, 1200);
+  } else if (joystickInput > upperBoundJS) {
+    desiredSpeedForward = map(joystickInput, upperBoundJS, 1024, 0, 1200);
     //Serial.println("pass forward");
+  } else {
+    //Serial.println("pass neutral");
   }
-  
+
+  // calculate actual speed using encoder
   timeIntervalL = (timeL2 - timeL1) / 1000000.0;
-  float currentSpeed = 360 / 12 / timeIntervalL;
+  float measuredSpeed = 360 / nHoles / timeIntervalL;
 
-//  Serial.print("desired speed forwards: ");
-//  Serial.print(desiredSpeedBackward);
-//  Serial.print(",");
-//  Serial.print("desired speed backwards: ");
-//  Serial.print(desiredSpeedBackward);
-//  Serial.print(",");
-//  Serial.print("current speed: ");
-//  Serial.println(currentSpeed);
+  //validity check
+  if (measuredSpeed > 0 && measuredSpeed < 7000) {
+    circularBufferL.push(measuredSpeed);
+    float currentSpeed = 0;
+    for (int i = 0; i < bufferSize; i++) {
+      currentSpeed += circularBufferL[i];
+    }
+    currentSpeed = currentSpeed / bufferSize;
+    Serial.println(currentSpeed);
+    //float currentSpeed = measuredSpeed;
 
+    timeInt1 = timeInt2;
+    timeInt2 = micros();
+    unsigned long timeIntervalInt = (timeInt2 - timeInt1) / 1000000.0;
+
+    // Proportional
+    errorPreviousL = errorL;
+    if (desiredSpeedForward != 0) {
+      errorL = desiredSpeedForward - currentSpeed;
+    } else if (desiredSpeedBackward != 0) {
+      errorL = desiredSpeedBackward - currentSpeed;
+    } else {
+      errorL = 0 - currentSpeed;
+    }
+    float PIDp = LKp * errorL;
+
+    // Integral
+    integralL += ((errorL + errorPreviousL) / 2.0) * (timeIntervalInt);
+    float PIDi = LKi * integralL;
+
+    // Derivative
+    //derivativeL = (errorL - errorPreviousL) / (timeIntervalInt);
+    //float PIDd = LKd * derivativeL;
+
+    //return total PID-control value
+    total = PIDp + PIDi;
+    //+ PIDd;
+
+//    Serial.print("Desired:");   Serial.print(desiredSpeedForward);  Serial.print(", ");
+//    Serial.print("Measured:");  Serial.print(measuredSpeed);        Serial.print(", ");
+//    Serial.print("Current:");   Serial.print(currentSpeed);         Serial.print(", ");
+//    Serial.print("PID:");       Serial.println(total);                //Serial.print(", ");
+
+    //    Serial.print("desiredSpeedForward: "); Serial.println(desiredSpeedForward);
+    //    Serial.print("currentSpeed: "); Serial.println(currentSpeed);
+    //    Serial.print("errorPreviousL: "); Serial.println(errorPreviousL);
+    //    Serial.print("errorL: "); Serial.println(errorL);
+    //    Serial.print("PIDp: "); Serial.println(PIDp);
+    //    Serial.print("PIDi: "); Serial.println(PIDi);
+    //    //Serial.print("PIDd: "); Serial.println(PIDd);
+    //    Serial.print("Total: "); Serial.println(total);
+  }
+
+  //  Serial.print("desired speed forwards: ");
+  //  Serial.print(desiredSpeedBackward);
+  //  Serial.print(",");
+  //  Serial.print("desired speed backwards: ");
+  //  Serial.print(desiredSpeedBackward);
+  //  Serial.print(",");
+  //  Serial.print("current speed: ");
+  //  Serial.println(currentSpeed);
+
+  // apply appropriate power to forward or backward pin depending on desired speed and direction
   if (desiredSpeedForward != 0) {                                                         //desired speed vooruit niet nul dus je wil vooruitrijden
-    int requiredPower = map(desiredSpeedForward, 0, 1200, 0, 255);
-    //int properPower = constrain(desiredSpeed, 0, 255);
-    analogWrite(forwardPinL, requiredPower);
+    //int requiredPower = map(desiredSpeedForward, 0, 1200, 80, 255);
+    int requiredPower = total;
+    int properPower = constrain(requiredPower, minimumPower, 255);
+    analogWrite(forwardPinL, properPower);
     analogWrite(backwardPinL, 0);
+    //Serial.print("properpower:"); Serial.println(properPower);
+    //Serial.println(properPower);
     //Serial.println("vooruit");
   } else if (desiredSpeedBackward != 0) {                                                 //desired speed achteruit niet nul dus je wil achteruitrijden
-    int requiredPower = map(desiredSpeedBackward, 0, 1200, 0, 255);
-    //int properPower = constrain(desiredSpeed, 0, 255);
+    //int requiredPower = map(desiredSpeedBackward, 0, 1200, 80, 255);
+    int requiredPower = total;
+    int properPower = constrain(requiredPower, minimumPower, 255);
     analogWrite(forwardPinL, 0);
-    analogWrite(backwardPinL, requiredPower);
+    analogWrite(backwardPinL, properPower);
+    //Serial.println(properPower);
     //Serial.println("achteruit");
-  } else {
+  } else {                                                                                //beide zijn nul, dus doe niks
     analogWrite(forwardPinL, 0);
     analogWrite(backwardPinL, 0);
     //Serial.println("neutral");
@@ -85,6 +178,8 @@ void loop() {
   //analogWrite(9, 0);
   //Serial.print(F("left: ")); Serial.println(analogRead(sensorPinL));
   //Serial.print(F("right: ")); Serial.println(analogRead(sensorPinR));
+
+  delay(2);
 }
 
 void interruptL() {
