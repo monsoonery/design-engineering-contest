b3a9eb0b5d2d75c578753d4452ae963012b6a11b
monsoonery <masonchammat@gmail.com>
2022-03-23 23:09:47 +0100

    Added more testing functions

--- component_tests/continuous_servo/continuous_servo.ino
+++ component_tests/continuous_servo/continuous_servo.ino
@@ -5,21 +5,24 @@
 
 ServoDriver servo;                            // servodriver object
 
-const int servoPin = 1;                       // pin number on DRIVER, NOT on Arduino board
-const int servoPin2 = 2;                      // pin number on DRIVER, NOT on Arduino board
+const int servoPin = 7;                       // pin number on DRIVER, NOT on Arduino board
+const int servoPin2 = 11;                      // pin number on DRIVER, NOT on Arduino board
 const int potPin = A6;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();                               // join I2C bus (I2Cdev library doesn't do this automatically)
   servo.init(0x7f);                           // init driver address
-  servo.setServoPulseRange(500, 2500, 360);   // pulse range for our servos
+  //servo.setServoPulseRange(500, 2500, 360);   // pulse range for our servos
 }
 
 void loop() {
   //uncomment the code you want to use
   //demo();
-  potControl();
+  //potControl();
+  demo180();
+  //demoSafe();
+  //servo.setAngle(servoPin, 0);
 }
 
 void demo() {
@@ -43,7 +46,27 @@ void demo() {
   delay(1000);
 }
 
+void demo180() {
+  int delayz = 10;
+  for (int i = 0; i <= 180; i++) {
+    servo.setAngle(servoPin, i);                     // loop through all values
+    servo.setAngle(servoPin2, i);
+    Serial.println(i);
+    delay(delayz);
+  }
+  for (int i = 180; i >= 0; i--) {
+    servo.setAngle(servoPin, i);                     // loop through all values
+    servo.setAngle(servoPin2, i);
+    Serial.println(i);
+    delay(delayz);
+  }
+}
+
 void potControl() {
   int value = map(analogRead(potPin), 0, 1023, 90, 270);
   servo.setAngle(servoPin, value);
+}
+
+void demoSafe() {
+  servo.setAngle(servoPin, 90);
 }
